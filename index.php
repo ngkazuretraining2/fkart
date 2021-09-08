f2a35f00dca650cefd83bfdcfb7de6ad414621bb
ngkazuretraining2 <90232540+ngkazuretraining2@users.noreply.github.com>
2021-09-08 02:40:07 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-$connect = mysqli_connect("fkartbyngk.mysql.database.azure.com", "ngkroot", "ind@99999999", "fkart");
+$connect = mysqli_connect("fkartbyngk.mysql.database.azure.com", "ngkroot@fkartbyngk", "ind@99999999", "fkart");
 
 if(isset($_POST["add_to_cart"]))
 {
